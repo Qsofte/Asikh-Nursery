364038135a45bcd9364dd575077b33b91c3fb0b0
Aman Kumar Singh <aman@qsofte.com>
2025-03-05 14:39:07 +0530

    update

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -2,12 +2,15 @@ import "./Page.css";
 import aboutImg1 from "../images/About-img1.png";
 import aboutImg2 from "../images/About-img2.jpeg";
 import aboutImg3 from "../images/About-img3.jpeg";
-// import aboutImg4 from "../images/About-img4.png";
+import aboutImg4 from "../images/About-img4.png";
 
 const About = () => {
   return (
     <>
     <div className="abt-container">
+
+    {/* First Section */}
+
       <div className="abt-1">
         <div className="abt-img1">
         <img src={aboutImg1} alt="Nature Img" />
@@ -17,6 +20,8 @@ const About = () => {
           <p>About Us</p>
         </div>
       </div>
+
+    {/* Second Section */}
 
       <div className="abt-2">
         <div className="abt-img2">
@@ -30,11 +35,30 @@ const About = () => {
         </div>
       </div>
 
+    {/* Third Section */}
+
       <div className="abt-img3">
         <div className="abt-img3">
           <img src={aboutImg3} alt="Nursery Img" />
         </div>
       </div>
+
+    {/* Fourth Section */}
+
+      <div className="abt-2">
+        <div className="abt-content">
+          <h2>OUR MISSION</h2>
+          <p>
+            Tincidunt lacus lorem fringilla neque fringilla sit dictumst nunc. Ultrices tortor tellus tortor duis faucibus.
+          </p>
+        </div>
+
+        <div className="abt-img2">
+          <img src={aboutImg4} alt="Greenhouse" />
+        </div>
+      </div>
+
+
     </div>
     </>
   );
